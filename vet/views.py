de2acdaa5606b9f99273f66cba925e9f315c702e
Irving Suarez <irving.dev@icloud.com>
2023-10-30 20:09:06 -0600

    [06_09]: Adding views and urls

--- vet/views.py
+++ vet/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from vet.models import PetOwner
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def list_pet_owners(request):
+    owners = PetOwner.objects.all()
+
+    context = {"owners": owners}
+
+    return render(request, "vet/owners/list.html", context)
